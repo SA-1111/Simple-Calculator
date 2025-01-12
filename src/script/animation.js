5d96eb0dac5cd1c98c79939486db3c37c1afd86a
SA-1111 <saadahmadworks@gmail.com>
2025-01-12 20:28:35 +0530

    calculator log history functionality done!

--- src/script/animation.js
+++ src/script/animation.js
@@ -1,32 +1,33 @@
 import JSConfetti from 'js-confetti';
 
-const canvas = document.getElementById('bob')
-const jsConfetti = new JSConfetti({ canvas })
-
-function boom() {
-    jsConfetti.addConfetti({
-        confettiColors: [
-            '#ffa500'
-        ],
-        confettiRadius: 10,
-        confettiNumber: 100,
-    })
-}
-function sadboom() {
-    jsConfetti.addConfetti({
-        emojis:[
-            "😑",
-        ],
-        emojiSize: 50,
-        confettiNumber: 1,
-    })
-}
-
-window.boom = boom;
-window.sadboom = sadboom;
-
-
 document.addEventListener('DOMContentLoaded', () => {
+
+    const canvas = document.getElementById('bob')
+    const jsConfetti = new JSConfetti({ canvas })
+
+    function boom() {
+        jsConfetti.addConfetti({
+            confettiColors: [
+                '#ffa500'
+            ],
+            confettiRadius: 10,
+            confettiNumber: 100,
+        })
+    }
+
+    function sadboom() {
+        jsConfetti.addConfetti({
+            emojis: [
+                "🚫",
+            ],
+            emojiSize: 50,
+            confettiNumber: 15,
+        })
+    }
+
+    window.boom = boom;
+    window.sadboom = sadboom;
+    
     const spans = document.querySelectorAll('.special');
     spans.forEach((span, index) => {
         const delay = index * 500; // 500ms delay between each element
--- src/script/calculator.js
+++ src/script/calculator.js
@@ -1,3 +1,13 @@
+let calculation = [];
+if (localStorage.getItem('calculation') !== null) {
+    const storedCalculation = JSON.parse(localStorage.getItem('calculation'));
+    if (Array.isArray(storedCalculation)) {
+        calculation = storedCalculation;
+        console.log('calculation', calculation);
+    }
+}
+
+let newCalculation = {};
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -5,56 +15,100 @@ document.addEventListener('DOMContentLoaded', () => {
     const outputField = document.getElementById('output');
     const buttons = document.querySelectorAll('.cal-btn');
 
+    const histbtn = document.getElementById('history_btn');
+
+
     // Add click event listener to all buttons
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => handleButtonClick(e, inputField, outputField));
     });
-
-    function handleButtonClick(event, inputField, outputField) {
-        const buttonText = event.target.textContent;
-
-        // Handle Clear (C)
-        if (buttonText === 'C') {
-            clearFields(inputField, outputField);
-            return;
-        }
-
-        // Handle Delete (del)
-        if (buttonText === 'del') {
-            deleteLastCharacter(inputField);
-            return;
-        }
-
-        // Handle Equal (=)
-        if (buttonText === '=') {
-            calculateResult(inputField, outputField);
-            return;
-        }
-        
-        // Otherwise, add the clicked button's value to the input field
-        appendToInput(inputField, buttonText);
-    }
     
-    function clearFields(inputField, outputField) {
-        inputField.value = '';
-        outputField.value = '';
-    }
+
     
-    function deleteLastCharacter(inputField) {
-        inputField.value = inputField.value.slice(0, -1);
-    }
-    
-    function appendToInput(inputField, text) {
-        inputField.value += text;
-    }
-    
-    function calculateResult(inputField, outputField) {
-        try {
-            outputField.value = math.evaluate(inputField.value);
-            eval(`boom()`);
-        } catch (error) {
-            outputField.value = 'Error';
-            eval(`sadboom()`);
-        }
-    }
 });
+
+function handleButtonClick(event, inputField, outputField) {
+    const buttonText = event.target.textContent;
+
+    // Handle Clear (C)
+    if (buttonText === 'C') {
+        clearFields(inputField, outputField);
+        return;
+    }
+
+    // Handle Delete (del)
+    if (buttonText === 'del') {
+        deleteLastCharacter(inputField);
+        return;
+    }
+
+    // Handle Equal (=)
+    if (buttonText === '=') {
+        calculateResult(inputField, outputField);
+        return;
+    }
+
+    // Otherwise, add the clicked button's value to the input field
+    appendToInput(inputField, buttonText);
+}
+
+function calculate(input) {
+    input = input.replace("×", '*');
+    return math.evaluate(input);
+
+}
+
+function saveCalculation(str, result) {
+    newCalculation = {
+        input: str,
+        result: result
+    };
+
+    if (!calculation.some(cal => cal.input === str)) {
+        calculation.push(newCalculation);
+        localStorage.setItem('calculation', JSON.stringify(calculation));
+    }
+}
+
+
+function hasCalculation() {
+    return localStorage.getItem('calculation') !== null;
+}
+
+function clearFields(inputField, outputField) {
+    inputField.value = '';
+    outputField.value = '';
+}
+
+function deleteLastCharacter(inputField) {
+    inputField.value = inputField.value.slice(0, -1);
+}
+
+function appendToInput(inputField, text) {
+    inputField.value += text;
+}
+
+function calculateResult(inputField, outputField) {
+    try {
+        const result = calculate(inputField.value);
+        outputField.value = result;
+        saveCalculation(inputField.value, result);
+        // eval(`boom()`);
+    } catch (error) {
+        console.log(error);
+        outputField.value = 'Error';
+        // eval(`sadboom()`);
+    }
+}
+
+function clearHistory() {
+    calculation = {};
+    localStorage.removeItem("calculation");
+}
+
+function logHistory() {
+    console.log(JSON.parse(localStorage.getItem('calculation')));
+}
+
+window.logHistory = logHistory;
+window.clearHistory = clearHistory;
